11465d7e9a6d3e89ad32255919a7e96fb1babcb2
David Ferreira <davidleandro670@gmail.com>
2025-06-25 11:23:12 -0300

    Colocando a port para iniciar

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const app = express();
-const pool = require('./db');
+const pool = require('./dB');
+//--//
+app.use(express.json());
+
+// TODOS AS REQUISIÇÕES NAS APIs precisam ser LETRA MINUSCULA, SEU CABISBAIXO
+app.get('/series',async (_,res) => {
+    try {
+        const series = await pool.query('SELECT * FROM series');
+        res.status(200).json(series.rows);
+    } catch (err) {
+        console.error('erro ao buscar série', err);
+        res.status(500).json({error: 'Falha na busca de série'});
+    }
+});
+
+app.post('/series', async (req,res) => {
+    const { id, title, yearexhibition, gender_id, rating_id, season_id, status } = req.body;
+    try {
+        const series = await pool.query(
+            'INSERT INTO series(id, title, yearexhibition, gender_id, rating_id, season_id, status) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+             [id, title, yearexhibition, gender_id, rating_id, season_id, status]
+            );
+     res.status(201).json(series.rows[0]);
+    } catch (err) {
+        console.error('Erro ao adicionar série', err)
+        res.status(500).json({error: 'Falha ao adicionar uma série'})
+    }
+});
+
+app.put ('/series/:id', async(req,res) => {
+    const { id } = req.params;
+    const { title, yearexhibition, gender_id, rating_id, season_id, status } = req.body;
+
+    try {
+        const series = await pool.query(
+            'UPDATE series SET title = $1, yearexhibition = $2, gender_id = $3, rating_id = $4, season_id = $5, status = $6 WHERE id = $7'
+            [title, yearexhibition, gender_id, rating_id, season_id, status, id]
+        )
+        
+
+    if (series.rowcount === 0) {
+        return res.status(404).json ({error: 'Não existe esta série na DataBase'})
+    }   
+        res.status(200).json(series.rows[0]);
+    } catch (err) {
+    console.error('Falha ao adicionar tarefa, Verifique os dados e tente novamente',err)
+    res.status(500).json ({error: 'Erro ao adicionar serie no Banco' })
+    }
+});
+
+app.delete
 
 
-
+//--//
 const PORT = 3000;
 app.listen(PORT, ()=> {
    console.log (`Servidor iniciado na porta ${PORT}`);
